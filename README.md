146ef2898a447fd8fa1ef51ec2e5bc81adb60db7
Ememak <ememak@interia.pl>
2020-05-10 12:53:16 +0200

    Zmiana podpisów

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Projekt-Rada
 
 zrobiona inicjalizacja i głosowanie w ankiecie
-krytografia: zrobiony schemat ślepych podpisów; dużo dziur w stylu wielu rodzajów komunikatów zwrotnych, brak paddingu i obrony przed side channel atakami
-TODO: przechowywanie informacji
-TODO: szyfrowanie tokenów?
 
-TODO: porządne korzystanie z gita :)
+krytografia: zrobiony schemat ślepych podpisów; 
+dużo dziur w stylu wielu rodzajów komunikatów zwrotnych, brak paddingu i obrony przed side channel atakami
+
+Zmieniłem schemat podpisów na to, co Pan sugerował i wydaje się działać, choć należałoby to lepiej przetestować. Dodałem też trochę komentarzy w miejscach, które mogyfikowałem.
+
+Serwer szyfruje otrzymaną wiadomość kluczem publicznym w 124 linii. Na razie jest to zrobione wprost - sprawdziłem i wciąż uważam, że crypto nie ma dostępnych na zewnątrz funkcji do szyfrowania bez paddingu (a to może być problem gdy klient chce następnie przemnożyć wiadomość przez r^-1).
+
+Mogę wziąć z https://golang.org/src/crypto/rsa/rsa.go funkcję decrypt (robi to co chcę, ale nie jest widoczna na zewnątrz), znalazłem też bibliotekę https://github.com/cryptoballot/rsablind zajmującą się ślepymi podpisami (ostatni commit 3 lata temu...).
+
+TODO: przechowywanie informacji
--- client/main.go
+++ client/main.go
@@ -1,134 +1,168 @@
 package main
 
 import (
-  "context"
-  "time"
-  "fmt"
-  "log"
-  "io"
-  "crypto/rsa"
-  "crypto/rand"
-  "crypto/sha256"
-  "math/big"
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+	"math/big"
+	"time"
 
-  "google.golang.org/grpc"
-  pb "github.com/ememak/Projekt-Rada/query"
+	pb "github.com/ememak/Projekt-Rada/query"
+	"google.golang.org/grpc"
 )
 
 var (
-  addr = "localhost:12345"
+	addr = "localhost:12345"
 )
 
 var key *rsa.PublicKey
 
-func runQueryInit (client pb.QueryClient, ctx context.Context) {
-  ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-  defer cancel()
-  stream, err := client.QueryInit(ctx)
-  if err != nil {
-    log.Fatalf("%v.QueryInit(_) = _, %v", client, err)
-  }
-
-  for i := 0; i<len(exampleQuery); i++ {
-	  if err := stream.Send(&exampleQuery[i]); err != nil {
-		  if err == io.EOF {
-			  break
-		  }
-		  log.Fatalf("%v.Send(%v) = %v", stream, exampleQuery[i], err)
-	  }
-  }
-  reply, err := stream.CloseAndRecv()
-  if err != nil {
-	  log.Fatalf("%v.CloseAndRecv() got error %v", stream, err)
-  }
-  log.Printf("Query: %v", reply)
+func runHello(client pb.QueryClient, ctx context.Context) {
+	r, err := client.Hello(ctx, &pb.HelloRequest{})
+	if err != nil {
+		fmt.Printf("Client got error on Hello function: %v", err)
+	}
+	key = &rsa.PublicKey{
+		N: new(big.Int).SetBytes(r.GetN()),
+		E: int(r.GetE()),
+	}
 }
 
-func runHello(client pb.QueryClient, ctx context.Context){
-  r, err := client.Hello(ctx, &pb.HelloRequest{})
-  if err != nil {
-    fmt.Printf("Client got error on Hello function: %v", err)
-  }
-  key = &rsa.PublicKey{
-    N: new(big.Int).SetBytes(r.GetN()),
-    E: int(r.GetE()),
-  }
+func runQueryInit(client pb.QueryClient, ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	stream, err := client.QueryInit(ctx)
+	if err != nil {
+		log.Fatalf("%v.QueryInit(_) = _, %v", client, err)
+	}
+
+	for i := 0; i < len(exampleQuery); i++ {
+		if err := stream.Send(&exampleQuery[i]); err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("%v.Send(%v) = %v", stream, exampleQuery[i], err)
+		}
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got error %v", stream, err)
+	}
+	log.Printf("Query: %v", reply)
 }
 
-func runVote(client pb.QueryClient, ctx context.Context, vote pb.Vote){
-  t, err := client.QueryGetToken(ctx, &pb.TokenRequest{Nr: vote.Nr})
-  if err != nil {
-    fmt.Printf("Client got error on GetToken function: %v", err)
-  }
-  fmt.Printf("Token: %v\n", t)
+//Function to get token, generate blind signature and vote using it
+func runVote(client pb.QueryClient, ctx context.Context, vote pb.Vote) {
+	//get token to vote, may be changed
+	t, err := client.QueryGetToken(ctx, &pb.TokenRequest{Nr: vote.Nr})
+	if err != nil {
+		fmt.Printf("Client got error on GetToken function: %v", err)
+	}
+	fmt.Printf("Token: %v\n", t)
 
-  var hash = sha256.Sum256([]byte(fmt.Sprintf("%v", vote)))
-  r, err := rand.Int(rand.Reader, key.N);
-  if err != nil {
-    fmt.Printf("Rand.Int error: %v", err)
-  }
-  c := new(big.Int).SetBytes(hash[:])
-  bfactor := new(big.Int).Exp(r, big.NewInt(int64(key.E)), key.N)
-  blinded := bfactor.Mod(bfactor.Mul(bfactor, c), key.N).Bytes()
-  var mts = pb.MessageToSign{
-    Mess: blinded,
-    Nr: vote.Nr,
-    Token: t,
-  }
-  sm, err := client.QueryAuthorizeVote(ctx, &mts)
-  if err != nil {
-    fmt.Printf("Client got error on QueryVote function: %v", err)
-  }
+	//generate message to be signed
+	mess, err := rand.Int(rand.Reader, key.N)
+	if err != nil {
+		fmt.Printf("Rand.Int error in generating message to sign: %v", err)
+	}
+	fmt.Printf("m: %v\n", mess)
+	//hash this message
+	//from now on m is this hashed message
+	hash := sha256.Sum256(mess.Bytes())
+	m := new(big.Int).SetBytes(hash[:])
+	fmt.Printf("Hashed m: %v\n", m)
 
-  smi := new(big.Int).SetBytes(sm.Sign)
-  revr := new(big.Int).ModInverse(r, key.N)
-  smirevr := new(big.Int).Mul(revr, smi)
-  sign := new(big.Int).Mod(smirevr, key.N).Bytes()
-  var sv = pb.SignedVote{
-    Vote: &vote,
-    Sign: sign,
-  }
-  vr, err := client.QueryVote(ctx, &sv)
-  if err != nil {
-    fmt.Printf("Client got error on QueryVote function: %v", err)
-  }
-  fmt.Printf("Mess: %v\n", vr.Mess)
+	//get random blinding factor
+	r, err := rand.Int(rand.Reader, key.N)
+	if err != nil {
+		fmt.Printf("Rand.Int error in generating blinding factor: %v", err)
+	}
+
+	//we want to send m*r^e mod N to server
+	//r^e
+	bfactor := new(big.Int).Exp(r, big.NewInt(int64(key.E)), key.N)
+	//m*r^e mod N
+	blinded := bfactor.Mod(bfactor.Mul(bfactor, m), key.N)
+	//send m*r^e to server
+	//with number of query and token to it
+	var mts = pb.MessageToSign{
+		Mess:  blinded.Bytes(),
+		Nr:    vote.Nr,
+		Token: t,
+	}
+
+	//receive m^d*r from server
+	sm, err := client.QueryAuthorizeVote(ctx, &mts)
+	if err != nil {
+		fmt.Printf("Client got error on QueryVote function: %v", err)
+	}
+
+	//having m^d*r we are removing blinding factor r
+	//then we send vote with actual sign
+	//smi = m^d*r
+	smi := new(big.Int).SetBytes(sm.Sign)
+	//revr = r^-1 mod N
+	revr := new(big.Int).ModInverse(r, key.N)
+	//smirevr = smi * revr
+	smirevr := new(big.Int).Mul(revr, smi)
+	//sign = smirevr mod N = m^d mod N
+	//actual sign
+	sign := new(big.Int).Mod(smirevr, key.N).Bytes()
+
+	//send vote with pair m, hash(m)^d
+	var sv = pb.SignedVote{
+		Vote:   &vote,
+		Signm:  mess.Bytes(),
+		Signmd: sign,
+	}
+	vr, err := client.QueryVote(ctx, &sv)
+	if err != nil {
+		fmt.Printf("Client got error on QueryVote function: %v", err)
+	}
+	fmt.Printf("Mess: %v\n", vr.Mess)
 }
 
-func main(){
-  conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock())
-  if err != nil {
-    log.Fatalf("grpc.Dial got error %v", err)
-  }
-  defer conn.Close()
-  c:=pb.NewQueryClient(conn)
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("grpc.Dial got error %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewQueryClient(conn)
 
-  ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 
-  defer cancel()
+	defer cancel()
 
-  runHello(c, ctx)
+	runHello(c, ctx)
 
-  runQueryInit(c, ctx);
+	runQueryInit(c, ctx)
 
-  runVote(c, ctx, exampleVote0)
+	runVote(c, ctx, exampleVote0)
 
-  runVote(c, ctx, exampleVote1)
+	runVote(c, ctx, exampleVote1)
 }
 
 var exampleQuery = []pb.Field{
-    {Which: -1, Name: "First Option"},
-    {Which: -1, Name: "Second Option"},
-    {Which: 0, Name: "Edit First Option"},
-    {Which: -1, Name: "Third Option"},
-  }
+	{Which: -1, Name: "First Option"},
+	{Which: -1, Name: "Second Option"},
+	{Which: 0, Name: "Edit First Option"},
+	{Which: -1, Name: "Third Option"},
+}
 
+//first time launching the client first vote should pass, second not
+//second time both should pass
+//second vote is asking about query number one, which don't exist during first launch
 var exampleVote0 = pb.Vote{
-    Nr: 0,
-    Answer: []int32{0, 1, 1},
-  }
+	Nr:     0,
+	Answer: []int32{0, 1, 1},
+}
 
 var exampleVote1 = pb.Vote{
-    Nr: 1,
-    Answer: []int32{1, 1, 0},
-  }
+	Nr:     1,
+	Answer: []int32{1, 1, 0},
+}
